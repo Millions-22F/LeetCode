081759b8b1afd25424ce23003d299eadf65d8eb7
HJeong1200 <hjeong1200@gmail.com>
2023-12-10 15:54:16 +0900

    solve: 2주차 BinarySearch

---
+++ 2주차/704_BinarySearch_Hojeong.js
@@ -0,0 +1,15 @@
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const middle = Math.floor((left + right) / 2);
+
+    if (nums[middle] === target) return middle;
+
+    if (nums[middle] > target) right = middle;
+    if (nums[middle] < target) left = middle + 1;
+  }
+
+  return nums[left] === target ? left : -1;
+};
